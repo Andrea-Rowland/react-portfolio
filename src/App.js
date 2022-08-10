053bc8a05ed69a4860e19897b619de8730e86648
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-08-10 10:07:58 -0600

    added work links

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Skills from './components/Skills';
-import Gallery from './components/Gallery';
+import Work from './components/Work';
 
 function App() {
   return (
     <div><Header />
     <About />
     <Skills />
-    <Gallery />
+    <Work />
     </div>
   );
 }
---
+++ src/components/Work/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import strategicHome from "../../assets/work/Screenshot-main.JPG"
+import serenityNow from "../../assets/work/SN-screenshot.JPG"
+import runBuddy from "../../assets/work/run-buddy.JPG"
+
+
+function Work() {
+    
+    return (
+    <div>
+        <h1>My Work</h1>
+            <article class="first">
+                <a href="https://strategic-home-inspection.herokuapp.com/" target="_blank">
+                    <img src={strategicHome} className="my-2" style={{ width: "80%" }} alt="strategicHome" />
+                    <label for="first-sample">Strategic Investments Home Inspection</label>
+                </a>
+            </article>
+            <article>
+                <a href="https://andrea-rowland.github.io/serenity-now/" target="_blank">
+                <img src={serenityNow} className="my-2" style={{ width: "80%" }} alt="serenityNow" />
+                <label for="serenity-now" class="project-title">Serenity Now</label>
+                </a>
+            </article>
+            <article>
+                <a href="https://andrea-rowland.github.io/run-buddy/" target="_blank">
+                <img src={runBuddy} className="my-2" style={{ width: "80%" }} alt="runBuddy" />    
+                <label for="run-buddy" class="project-title">Run Buddy</label>
+                </a>
+            </article>
+    </div>
+    )
+}
+
+export default Work;
