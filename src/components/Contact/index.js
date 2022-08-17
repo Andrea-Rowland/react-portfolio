c9a9cef9e59f5cdbfe411b8f1b24e90b694cf375
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-08-17 11:57:09 -0600

    completed email validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,44 +1,82 @@
-import React from 'react';
-import { validateEmail } from '../../utils/helper';
+import React, { useState } from 'react';
+import { validEmail } from './Regex';
+import './contact.css';
 
 function Contact() {
-    
+    const [email, setEmail] = useState('');
+    const [emailErr, setEmailErr] = useState(false);
+
+    const validate = (e) => {
+        e.preventDefault();
+        if (!validEmail.test(email)) {
+           setEmailErr(true);
+        }
+     };
+
     return (
         <React.Fragment>
-    <div>
-        <h1>Contact Me</h1>
-        <form>
-            <label for="contact-name">Your Name</label>
-            <input type="text" id="contact-name" placeholder="Your Name" />
+            <section id="reach-out" className="contact flex=row">
+                <div className="flex-row">
+                    <h2 className="section-title secondary-border">
+                    </h2>
+                </div>
 
-            <label for="email">Email</label>
-            <input type="email" id="email" placeholder="Email" />
+                <div className="contact-info" id="contact-info">
+                    <div>
+                        <h3>Mailing Address</h3>
 
-            <label for="contact-message">Message</label>
-            <textarea id="contact-message" placeholder="Message"></textarea>
+                        <address>
+                            11243 Freestone Road <br />
+                            South Jordan, UT<br />
+                            84009<br />
+                            P: 801.201.1555<br />
+                            E: <a href="mailto:andreadrowland@gmail.com">andreadrowland@gmailcom</a><br />
+                            <a href="./assets/docs/Andrea Resume 2022.pdf" id="resume"><strong>Click here for my resume</strong></a>
+                        </address>
 
-            <button type="submit">Submit</button>
-        </form>           
-    </div>
+                    </div>
 
-    <div>
-        <h1>Feeling Social?</h1><br />
+                    <div className="contact-form">
+                        <h3>Contact Me</h3>
+                        <form>
+                            <label for="contact-name">Your Name</label>
+                            <input type="text" id="contact-name" placeholder="Your Name" required />
 
-        <p>Find me on these online spaces too!</p><br />
+                            <label for="contact-email">Your Email</label>
+                            <input type="text" id="contact-email" placeholder="Your Email" required onChange={(e) => setEmail(e.target.value)} />
 
-                <a href="https://www.facebook.com/andrea.r.haag" target="_blank" class="sm-links">
-                    <ion-icon name="logo-facebook"></ion-icon>
-                </a>
-                <a href="https://www.instagram.com/andrea.r.haag/" target="_blank" class="sm-links">
-                    <ion-icon name="logo-instagram"></ion-icon>
-                </a>
-                <a href="https://www.linkedin.com/in/andrea-haag-328354180/" target="_blank" class="sm-links">
-                    <ion-icon name="logo-linkedin"></ion-icon>
-                </a>
+                            <label for="contact-message">Message</label>
+                            <textarea id="contact-message" placeholder="Message" required ></textarea>
+                            {emailErr && <p>Your email is invalid</p>}
+                            <button onClick={validate}>Submit</button>
+                        </form>
+                        
+                    </div>
 
-    </div>
+                    <div className="feeling-social">
+                        <div>
+                            <h3>Feeling Social?</h3><br />
 
-    </React.Fragment>
+                            <p>Find me on these online spaces too!</p>
+                        </div>
+                        
+
+                        <a href="https://www.facebook.com/andrea.r.haag" target="_blank" class="sm-links">
+                            <ion-icon name="logo-facebook"></ion-icon>
+                        </a>
+                        <a href="https://www.instagram.com/andrea.r.haag/" target="_blank" class="sm-links">
+                            <ion-icon name="logo-instagram"></ion-icon>
+                        </a>
+                        <a href="https://www.linkedin.com/in/andrea-haag-328354180/" target="_blank" class="sm-links">
+                            <ion-icon name="logo-linkedin"></ion-icon>
+                        </a>
+                    </div>
+                </div>
+
+
+            </section>
+
+        </React.Fragment>
     )
 }
 
